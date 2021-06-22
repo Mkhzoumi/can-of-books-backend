cea6b02323ebfd1b48154860145c799580487e8e
mkhzoumi <zx.hammam@hotmail.com>
2021-06-22 23:33:51 +0300

    added delete functionality

--- controllers/Book.controller.js
+++ controllers/Book.controller.js
@@ -3,12 +3,12 @@
 const userModel = require('../models/User.model');
 
 
-const getBooks = (req , res) => {
-    const {email} = req.query;
-    userModel.findOne({email: email},(error , user)=>{
-        if (error){
+const getBooks = (req, res) => {
+    const { email } = req.query;
+    userModel.findOne({ email: email }, (error, user) => {
+        if (error) {
             res.send(error)
-        }else{
+        } else {
             res.json(user)
         }
     });
@@ -17,14 +17,14 @@ const getBooks = (req , res) => {
 
 
 
-const addBook = (req , res) => {
-    const {email, name ,description , status } = req.body;
+const addBook = (req, res) => {
+    const { email, name, description, status } = req.body;
     console.log(req.body)
-    userModel.findOne({email: email},(error , user)=>{
-        if (error){
+    userModel.findOne({ email: email }, (error, user) => {
+        if (error) {
             res.send(error)
-        }else{
-            user.Books.push({name : name , description : description , status :status})
+        } else {
+            user.Books.push({ name: name, description: description, status: status })
             user.save();
             res.json(user)
         }
@@ -32,6 +32,26 @@ const addBook = (req , res) => {
 }
 
 
+const deleteBook = (request, response) => {
+    console.log(request.params)
+    const bookIndex = request.params.book_idx;
+    const { email } = request.query;
+
+    userModel.findOne({ email: email }, (error, user) => {
+        if (error) {
+            response.send(error)
+        } else {
+            user.Books.splice(bookIndex, 1);
+            user.save();
+            response.send(user)
+        }
+
+    });
+}
+
+
+
 module.exports = {
-    getBooks , 
-    addBook }
+    getBooks,
+    addBook,
+    deleteBook}
